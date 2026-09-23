3b86044778081dce1b595c6569a9685ac500ab1e
syncops24-sketch <syncops24@gmail.com>
2026-09-23 17:14:05 +0200

    Report scanner version 1.1.5

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { createReport } from './report/report.js';
 
 const app = express();
 const semaphore = new Semaphore(env.maxConcurrentScans);
-const SCANNER_VERSION = '1.1.4';
+const SCANNER_VERSION = '1.1.5';
 
 function logScanEvent(payload) {
   console.log(JSON.stringify({
